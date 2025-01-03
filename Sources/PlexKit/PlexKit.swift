62e7ef9f41d77a6fba9ff2ca8e366dcd8efe4d6c
Jason <jnorris441@gmail.com>
2025-01-03 13:38:58 -0800

    optional cast...

--- Sources/PlexKit/PlexKit.swift
+++ Sources/PlexKit/PlexKit.swift
@@ -14,7 +14,7 @@ public final class Plex {
     private let sessionConfiguration: URLSessionConfiguration
     lazy var session = URLSession(
         configuration: sessionConfiguration,
-        delegate: self as! URLSessionDelegate, 
+        delegate: self as? URLSessionDelegate, 
         delegateQueue: nil
     )
 
